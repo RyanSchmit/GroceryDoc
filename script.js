ffe838e8f0909b01eab2caa53584f0f9ce2a6c52
Ryan Sch <ryan@Ryans-Air.home>
2020-10-18 11:56:39 -0400

    added adding items

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-const tasks = [
-	'buy milk',
-	'eat dinner',
-	'nail javascript',
-	'give feedback',
-];
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.addItems');
+const groceryList = document.querySelector('.items');
+const list = document.querySelectorAll('.items li'); 
+
+let listLenght = list.lenght;
+
+const generateTempalate = (item) => {
+  
+  const html = `<li>
+                  <input type="checkbox" id="item_${listLenght}">
+                  <label for="item_${listLenght}">
+                    <span class="check"></span>
+                    ${item}
+                  </label>
+                  <i class="fas fa-minus"></i>
+                </li>`
+  groceryList.innerHTML += html;
+};
+
+function addItems(e) {
+  e.preventDefault();
+  const item = submitForm.add.value.trim();
+  if (item.length) {
+    listLenght = listLenght + 1;
+    generateTempalate(item);
+    submitForm.reset();
+  }
+}
+
+submitForm.addEventListener('submit', addItems);
+addButton.addEventListener('click', addItems);
+
+function deleteItems(e) {
+  if (e.target.classList.contains('fa-minus')) {
+    e.target.parentElement.remove();
+  }
+}
+
+groceryList.addEventListener('click', deleteItems);
